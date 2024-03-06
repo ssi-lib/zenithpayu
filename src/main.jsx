bc6b0170414483850247bc22ad102be8604b0152
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2024-03-06 11:55:09 +0100

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StoreProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </StoreProvider>
