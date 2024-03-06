d478e3257aa68fce312f359e3b5d553958537122
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2024-03-06 12:31:21 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/swiftpayu/',
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/__test__/setup.js',
   },
-  base: '/swiftpayu/',
 });
