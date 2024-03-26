890283af180d705bb69fd244508f1ca3c8905b93
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2024-03-26 11:27:46 +0100

    removed a lib

--- src/pages/account/Admin.jsx
+++ src/pages/account/Admin.jsx
@@ -1,4 +1,3 @@
-import { Button } from '@ssi-lib/ssi';
 import { auth, db } from '../../../firebase';
 import { useFetchUsers, toggleUserStatus } from '../../hooks/useFetchUser';
 import { useGlobalStore } from '../../store/Context';
@@ -74,20 +73,20 @@ const AdminDashboard = () => {
   return (
     <div className="space-y-6 py-10">
       <div className="flex justify-end space-x-7">
-        <Button
-          label="Change Status"
-          btn_color="bg-pri"
-          btn_text_color="text-white"
-          btn_text_size="text-sm"
+        <button
+          type="button"
+          className="bg-pri text-white text-sm px-8 py-2 rounded-md"
           onClick={handleConfirm}
-        />
-        <Button
-          label="Assign Fund"
-          btn_color="bg-pri"
-          btn_text_color="text-white"
-          btn_text_size="text-sm"
+        >
+          Change Status
+        </button>
+        <button
+          type="button"
+          className="bg-pri text-white text-sm px-8 py-2 rounded-md"
           onClick={handleAssignFund}
-        />
+        >
+          Assign Fund
+        </button>
       </div>
       <div className="overflow-x-auto w-full">
         <p className="text-sm text-neutral mb-2">User List</p>
@@ -107,6 +106,9 @@ const AdminDashboard = () => {
               </th>
               <th className="px-3 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-r-lg whitespace-nowrap">
                 Status
+              </th>
+              <th className="px-3 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider rounded-r-lg whitespace-nowrap">
+                Fund
               </th>
             </tr>
           </thead>
@@ -171,7 +173,7 @@ const AdminDashboard = () => {
                     </span>
                   </span>
                 </td>
-                <td className="py-5 border-b border-gray-200 text-sm whitespace-nowrap px-3">
+                <td className="py-5 border-b border-gray-200 text-sm whitespace-nowrap px-3 rounded-r-lg">
                   <input
                     type="text"
                     name=""
